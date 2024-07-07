9406dcbf5420ebad4980d2e231b42309bda4f80a
Nagaev Evgeny <evn1054@gmail.com>
2024-07-07 14:46:00 +0300

    sprint_1 add aliases and port for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
+import path from 'path';
 
 export default defineConfig({
     root: '.',
     build: {
         outDir: 'dist'
+    },
+    resolve: {
+        alias: {
+            '@components': path.resolve(__dirname, './src/components'),
+            '@assets': path.resolve(__dirname, './src/assets'),
+            '@styles': path.resolve(__dirname, './src/styles')
+        }
+    },
+    server: {
+        port: 3000
     }
 })
